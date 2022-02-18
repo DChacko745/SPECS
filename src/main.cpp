d0ee19128692190a05486144a1fe502ac5b6569e
David Chacko <4davidchacko@gmail.com>
2022-02-18 16:52:26 -0500

    Updated code with four transistor output for motor

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include <Arduino.h>
 
-#define MOTOR_PIN_1 3
-#define MOTOR_PIN_2 4
+#define MOTOR_PIN_1 6
+#define MOTOR_PIN_2 7
+#define MOTOR_PIN_3 8
+#define MOTOR_PIN_4 9
 
 bool connectedToApp = false;
 
-int wait_time = 20; // time between cleanings
-int operation_time = 10; // time for a sweep in one direction
-int cooldown_time = 5; // minimum time for system to cooldown before next operation
+int wait_time = 7; // time between cleanings
+int operation_time = 5; // time for a sweep in one direction
+int cooldown_time = 3; // minimum time for system to cooldown before next operation
 
 float temperature_threshold; // minimum value for temperature sensor to accept its input
 float humidity_threshold; // minimum value for humidity sensor to accept its input
@@ -16,18 +18,25 @@ float daylight_threshold; // minimum value for daylight sensor to accept its inp
 
 void Clean() {
   digitalWrite(MOTOR_PIN_2, LOW);
+  digitalWrite(MOTOR_PIN_4, LOW);
   sleep(2);
   digitalWrite(MOTOR_PIN_1, HIGH);
+  digitalWrite(MOTOR_PIN_3, HIGH);
   sleep(operation_time); // Does this stop the operations?
   digitalWrite(MOTOR_PIN_1, LOW);
+  digitalWrite(MOTOR_PIN_3, LOW);
   sleep(2);
   digitalWrite(MOTOR_PIN_2, HIGH);
+  digitalWrite(MOTOR_PIN_4, HIGH);
   sleep(operation_time); // Does this stop the operations?
   digitalWrite(MOTOR_PIN_2, LOW);
+  digitalWrite(MOTOR_PIN_4, LOW);
   sleep(cooldown_time);
 }
 
 void ReadSensors() {
+  // Has to run while system is sleeping
+
   // Listens to sensors and calls Clean() if necessary
 
   /* if ((temperatureInput >= temperature_threshold) &&
@@ -52,6 +61,8 @@ void Disconnect() {
 }
 
 void CheckAppConnection() {
+  // Has to run while system is sleeping
+
   // Listen if app is on
   // If on, connect
   // Else, disconnect
@@ -62,8 +73,10 @@ void setup() {
   // Set pinModes
   pinMode(MOTOR_PIN_1, OUTPUT);
   pinMode(MOTOR_PIN_2, OUTPUT); 
+  pinMode(MOTOR_PIN_3, OUTPUT);
+  pinMode(MOTOR_PIN_4, OUTPUT); 
 
-
+  // CheckAppConnection()
 }
 
 void loop() {
