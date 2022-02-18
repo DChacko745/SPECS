7ae79488be4aa2f1bf5255da0ec3c28d1bf0d594
David Chacko <4davidchacko@gmail.com>
2022-02-18 14:22:58 -0500

    Added threshold variables for sensors

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,11 @@ int wait_time = 20; // time between cleanings
 int operation_time = 10; // time for a sweep in one direction
 int cooldown_time = 5; // minimum time for system to cooldown before next operation
 
+float temperature_threshold; // minimum value for temperature sensor to accept its input
+float humidity_threshold; // minimum value for humidity sensor to accept its input
+float rain_threshold; // minimum value for rain sensor to accept its input
+float daylight_threshold; // minimum value for daylight sensor to accept its input
+
 void Clean() {
   digitalWrite(MOTOR_PIN_2, LOW);
   sleep(2);
@@ -18,16 +23,24 @@ void Clean() {
   sleep(2);
   digitalWrite(MOTOR_PIN_2, HIGH);
   sleep(operation_time); // Does this stop the operations?
+  digitalWrite(MOTOR_PIN_2, LOW);
   sleep(cooldown_time);
 }
 
 void ReadSensors() {
   // Listens to sensors and calls Clean() if necessary
+
+  /* if ((temperatureInput >= temperature_threshold) &&
+        (humidityInput >= humidity_threshold) &&
+        (rainInput >= rain_threshold) &&
+        (daylightInput >= daylight_threshold)) {
+      Clean();
+    }
+  */
 }
 
 void UpdateSettings() {
   // Check if any changes to variables came from app
-
 }
 
 void Connect() {
